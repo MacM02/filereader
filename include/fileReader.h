76c36750cfaf4d01c1e7212991e36e3805889193
MACK MUIR-JEFFRYES <u1360490@umail.utah.edu>
2025-01-27 15:37:56 -0700

    Initial Setup Commit.

---
+++ include/fileReader.h
@@ -0,0 +1,21 @@
+#ifndef FILEREADER_H
+#define FILEREADER_H
+
+
+#include <iostream>
+#include <fstream>
+
+class fileReader {
+private:
+    std::ifstream fileStream;
+
+public:
+    fileReader(std::string fileName) {
+        
+    };
+
+
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+/**
+ * 
+ * 
+ * Mack Muir-Jeffryes
+ * January 27th, 2025
+ */
+
+int main(int argc, char *argv[]) {
+    
+}
